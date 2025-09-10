9b27fad6319d2e4e4988c3a81675f77ac75a15f7
BitSS22 <121800812+BitSS22@users.noreply.github.com>
2025-09-10 10:44:27 +0900

    초기 커밋.

---
+++ SampleProject/LinkedList.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 예외 처리 안함. 터지면 사용자 탓.
+public class LinkedList<Type>
+{
+    public class Iterator
+    {
+        public Iterator(LinkedList<Type> _Owner, node _Node)
+        {
+            Owner = _Owner;
+            ptr = _Node;
+        }
+
+        public LinkedList<Type> Owner = null!;
+        public node ptr = null!;
+
+        static public Iterator operator++(Iterator _this)
+        {
+            if (_this.Owner.DNode != _this.ptr)
+            {
+                _this.ptr = _this.ptr.Next;
+            }
+
+            return _this;
+        }
+        static public Iterator operator--(Iterator _this)
+        {
+            if (_this.Owner.DNode != _this.ptr.Prev)
+            {
+                _this.ptr = _this.ptr.Prev;
+            }
+
+            return _this;
+        }
+    }
+    public class node
+    {
+        public node(Type _Item)
+        {
+            Data = _Item;
+        }
+        public node Prev = null!;
+        public node Next = null!;
+        public Type Data = default!;
+    }
+
+    node DNode = new(default!);
+    public UInt64 size { get; private set; } = 0;
+
+    public void PushBack(Type _Item)
+    {
+        node NewNode = new(_Item);
+
+        node PrevTail = DNode.Prev;
+        PrevTail.Next = NewNode;
+        DNode.Prev = NewNode;
+
+        NewNode.Prev = PrevTail;
+        NewNode.Next = DNode;
+
+        ++size;
+    }
+
+    public void PushFront(Type _Item)
+    {
+        node NewNode = new(_Item);
+
+        node PrevHead = DNode.Next;
+        PrevHead.Prev = NewNode;
+        DNode.Next = NewNode;
+
+        NewNode.Next = PrevHead;
+        NewNode.Prev = DNode;
+
+        ++size;
+    }
+
+    public void Insert(Iterator _Where, Type _Item)
+    {
+        node NewNode = new(_Item);
+
+        node WNode = _Where.ptr;
+        node PNode = WNode.Prev;
+
+        PNode.Next = NewNode;
+        WNode.Prev = NewNode;
+
+        NewNode.Prev = PNode;
+        NewNode.Next = WNode;
+
+        ++size;
+    }
+
+    public void Erase(Iterator _Where)
+    {
+        node PNode = _Where.ptr.Prev;
+        node NNode = _Where.ptr.Next;
+
+        _Where.ptr.Prev = default!;
+        _Where.ptr.Next = default!;
+
+        PNode.Next = NNode;
+        NNode.Prev = PNode;
+
+        --size;
+    }
+
+    public Iterator Begin()
+    {
+        return new Iterator(this, DNode.Next);
+    }
+
+    public Iterator End()
+    {
+        return new Iterator(this, DNode);
+    }
+}
---
+++ SampleProject/List.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+// 예외 처리 안함. 터지면 사용자 탓.
+public class List<Type>
+{
+    Type[] arr = Array.Empty<Type>();
+    public System.UInt64 size { get; private set; } = 0;
+    public System.UInt64 capacity { get; private set; } = 0;
+    public Type this[UInt64 _Index]
+    {
+        get => arr[_Index];
+        set => arr[_Index] = value;
+    }
+
+    public void PushBack(Type _Item)
+    {
+        if (size >= capacity)
+        {
+            UInt64 NewCapacity = capacity == 0 ? 1 : capacity * 2;
+            ReAllocate(NewCapacity);
+        }
+
+        arr[size++] = _Item;
+    }
+
+    public void PopBack()
+    {
+        arr[--size] = default!;
+    }
+
+    public void Erase(UInt64 _Index)
+    {
+        --size;
+
+        for (UInt64 i = _Index; i < size; ++i)
+        {
+            arr[_Index] = arr[_Index + 1];
+        }
+
+        arr[size] = default!;
+    }
+
+    void ReAllocate(System.UInt64 _NewCapacity)
+    {
+        Type[] NewArr = new Type[_NewCapacity];
+
+        for (UInt64 i = 0; i < size; ++i)
+        {
+            NewArr[i] = arr[i];
+        }
+
+        arr = NewArr;
+        capacity = _NewCapacity;
+    }
+
+}
