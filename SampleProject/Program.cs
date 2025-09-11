4f392da91e3371d44bc36f9be5e79db11ffec973
BitSS22 <121800812+BitSS22@users.noreply.github.com>
2025-09-11 15:43:41 +0900

    dd

--- SampleProject/Program.cs
+++ SampleProject/Program.cs
@@ -4,18 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        LinkedList<int> l = new LinkedList<int>();
-        for (int i = 0; i < 10; ++i)
-        {
-            l.PushBack(i);
-        }
-
         
-
-        for (var iter = l.Begin(); iter != l.End(); ++iter)
-        {
-            Console.WriteLine(iter.ptr);
-        }
     }
 }
 
--- SampleProject/Utility.cs
+++ SampleProject/Utility.cs
@@ -12,4 +12,20 @@ internal class Utility
         _Left = _Right;
         _Right = Temp;
     }
+
+    public static void Add(int[,] _Map ,int _Y, int _X)
+    {
+        _add(_Map, _Y + 1, _X + 1);
+        _add(_Map, _Y - 1, _X + 1);
+        _add(_Map, _Y + 1, _X - 1);
+        _add(_Map, _Y - 1, _X - 1);
+    }
+
+    static void _add(int[,] _Map, int _Y, int _X)
+    {
+        if (_Y < 0 || _X < 0 || _Y >= _Map.GetLength(0) || _X >= _Map.GetLength(1))
+            return;
+        ++_Map[_Y, _X];
+    }
+
 }
