144f83e73884517883669cd5fd497b9d394d5dde
BitSS22 <121800812+BitSS22@users.noreply.github.com>
2025-09-11 09:52:57 +0900

    d

--- SampleProject/Program.cs
+++ SampleProject/Program.cs
@@ -4,7 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
+        LinkedList<int> l = new LinkedList<int>();
+        for (int i = 0; i < 10; ++i)
+        {
+            l.PushBack(i);
+        }
+
         
+
+        for (var iter = l.Begin(); iter != l.End(); ++iter)
+        {
+            Console.WriteLine(iter.ptr);
+        }
     }
 }
 
---
+++ SampleProject/Utility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Utility
+{
+    public static void Swap<T>(ref T _Left, ref T _Right)
+    {
+        T Temp = _Left;
+        _Left = _Right;
+        _Right = Temp;
+    }
+}
